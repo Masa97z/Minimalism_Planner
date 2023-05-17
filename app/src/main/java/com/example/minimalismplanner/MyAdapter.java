2a27537115319b946b3fc3b6aaf0a2e7d7776655
masa <fmc97masa@gmail.com>
2023-05-18 00:11:20 +0300

    edit in the progect

---
+++ app/src/main/java/com/example/minimalismplanner/MyAdapter.java
@@ -0,0 +1,4 @@
+package com.example.minimalismplanner;
+
+public class BaseAdapter {
+}
--- app/src/main/java/com/example/minimalismplanner/Ui/addFragment.java
+++ app/src/main/java/com/example/minimalismplanner/Ui/addFragment.java
@@ -1,19 +1,18 @@
 package com.example.minimalismplanner;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link List#newInstance} factory method to
+ * Use the {@link addFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class List extends Fragment {
+public class addFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +23,7 @@ public class List extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public List() {
+    public addFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +33,11 @@ public class List extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment List.
+     * @return A new instance of fragment addFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static List newInstance(String param1, String param2) {
-        List fragment = new List();
+    public static addFragment newInstance(String param1, String param2) {
+        addFragment fragment = new addFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +58,6 @@ public class List extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        return inflater.inflate(R.layout.fragment_add, container, false);
     }
 }
