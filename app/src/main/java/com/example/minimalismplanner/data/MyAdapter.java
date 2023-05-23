34d2871a0479c8dafa03b6c8fc39049c1fe7421c
masa <fmc97masa@gmail.com>
2023-05-23 15:53:23 +0300

    complete data base connaction and handlet with ui

---
+++ app/src/main/java/com/example/minimalismplanner/data/MyAdapter.java
@@ -0,0 +1,51 @@
+package com.example.minimalismplanner;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<String> data; // Replace String with your data model
+
+    public MyAdapter(List<String> data) {
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String item = data.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.titleView);
+        }
+
+        public void bind(String item) {
+            textView.setText(item);
+        }
+    }
+}
